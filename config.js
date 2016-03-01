6130ad5cca3defd4e515391b39e8015c19752767
Vitalii_Menshutin <Vitalii_Menshutin@epam.com>
2016-03-01 11:40:57 +0200

    add: added support for V2 (test mode)

--- config.js
+++ config.js
@@ -544,6 +544,422 @@ var CONFIG = {
             ]
         }
     ],
+    "discovery.v2.events.get": [
+        { // subcolumn
+            "title": "Events", // subcolumn title (required)
+            "path": "_embedded.events", // path to fields (required)
+            "collection": true, // if array (not required)
+            "fields": [ // if collection is true there should be only 1 field to iterate through (required)
+                {
+                    "id": "name",
+                    "expandsTo": [
+                        { // subcolumn
+                            "title": "Event",
+                            "path": "_embedded.events",
+                            "expandsTo": "discovery.events.id.get",
+                            "fields": [
+                                {
+                                    "id": "id"
+                                },
+                                {
+                                    "id": "locale"
+                                },
+                                {
+                                    "id": "name"
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Dates",
+                            "path": "_embedded.events",
+                            "fields": [
+                                {
+                                    "id": "localDate",
+                                    "path": "dates.start"
+                                },
+                                {
+                                    "id": "localDate",
+                                    "path": "dates.end"
+                                },
+                                {
+                                    "id": "dates.timeZone"
+                                },
+                                {
+                                    "id": "code",
+                                    "path": "dates.status"
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Venues",
+                            "path": "_embedded.events",
+                            "collection": true,
+                            "fields": [
+                                {
+                                    "id": "name",
+                                    "path": "_embedded.venue",
+                                    "expandsTo": venueDetails
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Categories",
+                            "path": "_embedded.events",
+                            "collection": true,
+                            "fields": [
+                                {
+                                    "id": "name",
+                                    "path": "_embedded.categories",
+                                    "expandsTo" : categoryDetails
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Attractions",
+                            "path": "_embedded.events",
+                            "collection": true,
+                            "fields": [
+                                {
+                                    "id": "name",
+                                    "path": "_embedded.attractions",
+                                    "expandsTo": attractionDetails
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        pageDetails
+    ],
+    "discovery.v2.events.id.get": [
+        {
+            "title": "Events details", // subcolumn title (required)
+            "path": "", // path to fields (required)
+            "fields": [ // if collection is true there should be only 1 field to iterate through (required)
+                {
+                    "id": "id"
+                },
+                {
+                    "id": "locale"
+                },
+                {
+                    "id": "name"
+                },
+                {
+                    "id": "0",
+                    "path" : "promoterId"
+                }
+            ]
+        },
+        {
+            "title": "Dates",
+            "path": "dates",
+            "fields": [
+                {
+                    "id": "localDate",
+                    "path": "start"
+                },
+                {
+                    "id": "timezone"
+                },
+                {
+                    "id": "code",
+                    "path": "status"
+                }
+            ]
+        },
+        {
+            "title": "Venues",
+            "path": "_embedded.venue",
+            "collection": true,
+            "fields": [
+                {
+                    "id": "name",
+                    "expandsTo": venueDetails
+                }
+            ]
+        },
+        {
+            "title": "Categories",
+            "path": "_embedded.categories",
+            "collection": true,
+            "fields": [
+                {
+                    "id": "name",
+                    "expandsTo": categoryDetails
+                }
+            ]
+        },
+        {
+            "title": "Attractions",
+            "path": "_embedded.attractions",
+            "collection": true,
+            "fields": [
+                {
+                    "id": "name",
+                    "expandsTo": attractionDetails
+                }
+            ]
+        },
+        {
+            "title": "Get Images",
+            "path": "",
+            "expandsTo": "discovery.events.id.images.get"
+        }
+    ],
+    "discovery.v2.events.id.images.get": [
+        {
+            "title": "Event", // subcolumn title (required)
+            "path": "", // path to fields (required)
+            "fields": [ // if collection is true there should be only 1 field to iterate through (required)
+                {
+                    "id": "id"
+                }
+            ]
+        },
+        {
+            "title": "Images", // subcolumn title (required)
+            "path": "images", // path to fields (required)
+            "collection": true,
+            "fields": [ // if collection is true there should be only 1 field to iterate through (required)
+                {
+                    "id": "ratio",
+                    "thumbnail" : { // if thumbnail is shown. if this field is present - image pop-up will show on click
+                        "id" : "url", // name of the field with url
+                        "path" : '' // path to field with src
+                    },
+                    "expandsTo": [
+                        {
+                            "title": "Image", // subcolumn title (required)
+                            "path": "images", // path to fields (required)
+                            "fields": [ // if collection is true there should be only 1 field to iterate through (required)
+                                {
+                                    "id": "ratio",
+                                    "thumbnail" : { // if thumbnail is shown. if this field is present - image pop-up will show on click
+                                        "id" : "url", // name of the field with url
+                                        "path" : '' // path to field with src
+                                    }
+                                },
+                                {
+                                    "id": "url"
+                                },
+                                {
+                                    "id": "height"
+                                },
+                                {
+                                    "id": "width"
+                                },
+                                {
+                                    "id": "fallback"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "discovery.v2.attractions.get": [
+        {
+            "title": "Attractions", // subcolumn title (required)
+            "path": "_embedded.attractions", // path to fields (required)
+            "collection": true,
+            "fields": [ // if collection is true there should be only 1 field to iterate through (required)
+                {
+                    "id": "name",
+                    "expandsTo": attractionDetails
+                }
+            ]
+        },
+        pageDetails
+    ],
+    "discovery.v2.attractions.id.get": [
+        {
+            "title": "Attraction Details", // subcolumn title (required)
+            "path": "", // path to fields (required)
+            "fields": [ // if collection is true there should be only 1 field to iterate through (required)
+                {
+                    "id": "locale"
+                },
+                {
+                    "id": "name"
+                },
+                {
+                    "id": "id"
+                }
+            ]
+        }
+    ],
+    "discovery.v2.categories.get": [
+        {
+            "title": "Categories", // subcolumn title (required)
+            "path": "_embedded.categories", // path to fields (required)
+            "collection": true,
+            "fields": [ // if collection is true there should be only 1 field to iterate through (required)
+                {
+                    "id": "name",
+                    "expandsTo": categoryDetails
+                }
+            ]
+        },
+        pageDetails
+    ],
+    "discovery.v2.categories.id.get": [
+        {
+            "title": "Category",
+            "path": "",
+            "fields": [
+                {
+                    "id": "id"
+                },
+                {
+                    "id": "level"
+                },
+                {
+                    "id": "name"
+                },
+                {
+                    "id": "locale"
+                }
+            ]
+        }
+    ],
+    "discovery.v2.venues.get": [
+        {
+            "title": "Venues",
+            "path": "_embedded.venues", // venueS (not venue)
+            "collection": true,
+            "fields": [
+                {
+                    "id": "name",
+                    "expandsTo": [
+                        {
+                            "title": "Venue",
+                            "path": "_embedded.venues",
+                            "expandsTo": "discovery.venues.id.get",
+                            "fields": [
+                                {
+                                    "id": "id"
+                                },
+                                {
+                                    "id": "name"
+                                },
+                                {
+                                    "id": "postalCode"
+                                },
+                                {
+                                    "id": "timeZone"
+                                }
+                            ]
+                        },
+                        {
+                            "title": "Address",
+                            "path": "_embedded.venues",
+                            "map": { // available only for subcolumn (not collections)
+                                "coordinates" : {
+                                    "latitude" : "latitude",
+                                    "longitude" : "longitude",
+                                    "path" : "location"
+                                },
+                                "address" : [ // if lat or long are not specified, map will be generated by address
+                                    {
+                                        "id" : "line1",
+                                        "path" : "address"
+                                    },
+                                    {
+                                        "id" : "line2",
+                                        "path" : "address"
+                                    }
+                                ]
+                            },
+                            "fields": [
+                                {
+                                    "id": "line1",
+                                    "path": "address"
+                                },
+                                {
+                                    "id": "line2",
+                                    "path": "address"
+                                },
+                                {
+                                    "id": "name",
+                                    "path": "city"
+                                },
+                                {
+                                    "id": "countryCode",
+                                    "path": "country"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        pageDetails
+    ],
+    "discovery.v2.venues.id.get": [
+        {
+            "title": "Venue",
+            "path": "",
+            "fields": [
+                {
+                    "id": "id"
+                },
+                {
+                    "id": "name"
+                },
+                {
+                    "id": "postalCode"
+                },
+                {
+                    "id": "timeZone"
+                },
+                {
+                    "id": "locale"
+                }
+            ]
+        },
+        {
+            "title": "Address",
+            "path": "",
+            "map": { // available only for subcolumn (not collections)
+                "coordinates" : {
+                    "latitude" : "latitude",
+                    "longitude" : "longitude",
+                    "path" : "location"
+                },
+                "address" : [ // if lat or long are not specified, map will be generated by address
+                    {
+                        "id" : "line1",
+                        "path" : "address"
+                    },
+                    {
+                        "id" : "line2",
+                        "path" : "address"
+                    }
+                ]
+            },
+            "fields": [
+                {
+                    "id": "line1",
+                    "path": "address"
+                },
+                {
+                    "id": "line2",
+                    "path": "address"
+                },
+                {
+                    "id": "name",
+                    "path": "city"
+                },
+                {
+                    "id": "countryCode",
+                    "path": "country"
+                }
+            ]
+        }
+    ],
     "commerce.events.offers": {},
     "tap.events": {}
 };
